6ff67b1a6ad06b18e5efedc3de6e1a3c0ba30720
mir_faisal <mirfaisal08@gmail.comm>
2023-07-20 12:43:47 +0600

    changed logo and added social link

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -37,7 +37,7 @@
         <!-- favicon -->
         <link
             rel="shortcut icon"
-            href="{{ asset('favicon.png') }}"
+            href="{{ asset('favicon.jpg') }}"
             type="image/x-icon"
         />
 
@@ -53,7 +53,7 @@
     </head>
     <body class="relative">
         <!-- navigation start-->
-        <header id="nav" class="border-b fixed top-0 w-screen z-50">
+        <header id="nav" class="border-b fixed top-0 w-screen z-50 hidden">
             <div
                 class="mx-auto flex justify-between h-14 max-w-screen-xl items-center gap-8 px-2 md:px-0"
             >
@@ -72,7 +72,6 @@
                         <ul
                             class="flex flex-col md:flex-row items-center gap-6 text-md md:text-sm"
                         >
-                        
                             <li>
                                 <a
                                     class="nav-link transition py-1"
@@ -132,6 +131,83 @@
                 </div>
             </div>
         </header>
+        <!--  -->
+        <!--  -->
+        <nav class="border-gray-200 fixed top-0 w-screen z-50 dark:bg-gray-900">
+            <div
+                class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+            >
+                <a href="/" class="flex items-center">
+                    <img
+                        src="{{ asset('logo/logo.png') }}"
+                        class="w-32 mr-3"
+                        alt="Innova infosys"
+                    />
+                </a>
+                <button
+                    data-collapse-toggle="navbar-default"
+                    type="button"
+                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-default"
+                    aria-expanded="false"
+                >
+                    <span class="sr-only">Open main menu</span>
+                    <svg
+                        class="w-5 h-5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 17 14"
+                    >
+                        <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M1 1h15M1 7h15M1 13h15"
+                        />
+                    </svg>
+                </button>
+                <div
+                    class="hidden w-full md:block md:w-auto"
+                    id="navbar-default"
+                >
+                    <ul
+                        class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+                    >
+                        <li>
+                            <a
+                                href="{{ url('') }}#hero-section"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                aria-current="page"
+                                >Home</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                href="{{ url('') }}#solution"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                >Solution</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                href="{{ url('') }}#industries"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                >Industries</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                href="{{ route('CONTACT') }}"
+                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                >Contact</a
+                            >
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
         @yield('content')
 
@@ -869,7 +945,50 @@
                             </div>
 
                             <div>
-                                <p class="font-medium text-gray-900">Company</p>
+                                <p class="font-medium text-gray-900">
+                                    Follow us
+                                </p>
+
+                                <ul class="mt-6 space-y-4 text-sm">
+                                    <li>
+                                        <a
+                                            href="https://www.facebook.com/profile.php?id=100094676550997"
+                                            class="text-gray-700 transition hover:opacity-75"
+                                        >
+                                            Facebook
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://twitter.com/innovainfosys"
+                                            class="text-gray-700 transition hover:opacity-75"
+                                        >
+                                            Twitter
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://www.instagram.com/innovainfosys/"
+                                            class="text-gray-700 transition hover:opacity-75"
+                                        >
+                                            Instagram
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://www.quora.com/profile/II-256"
+                                            class="text-gray-700 transition hover:opacity-75"
+                                        >
+                                            Quora
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div>
+                                <p class="font-medium text-gray-900">
+                                    Helpful Links
+                                </p>
 
                                 <ul class="mt-6 space-y-4 text-sm">
                                     <li>
@@ -880,24 +999,6 @@
                                             About
                                         </a>
                                     </li>
-
-                                    <!-- <li>
-                                        <a
-                                            href="#"
-                                            class="text-gray-700 transition hover:opacity-75"
-                                        >
-                                            Meet the Team
-                                        </a>
-                                    </li> -->
-                                </ul>
-                            </div>
-
-                            <div>
-                                <p class="font-medium text-gray-900">
-                                    Helpful Links
-                                </p>
-
-                                <ul class="mt-6 space-y-4 text-sm">
                                     <li>
                                         <a
                                             href="#"
@@ -992,7 +1093,7 @@
                 },
             });
         </script>
-        <script>
+        <!-- <script>
             // Grab HTML Elements
             const btn = document.querySelector("#nav-button");
             const menu = document.querySelector(".mobile-menu");
@@ -1002,6 +1103,7 @@
                 console.log("click");
                 menu.classList.toggle("hidden");
             });
-        </script>
+        </script> -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
     </body>
 </html>
